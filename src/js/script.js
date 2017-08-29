b67c5be53e5a188a9d2ea4074132ef33685b458f
irie-shinnosuke <irie-shinnosuke@kayac.com>
2017-08-29 16:59:32 +0900

    comment out

--- src/js/script.js
+++ src/js/script.js
@@ -33,7 +33,7 @@ function handleFileSelect(evt) {
 
         fetch('https://api.webempath.net/v1/analyzeWav', {
             method: 'POST',
-            headers: myHeaders,
+            //headers: myHeaders,
             body: formData,
             mode: 'cors'
         })
